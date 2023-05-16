f5949fd8d9b455d6ab3f9c9f32b682f806add7cf
MetaverseUnknower <96612915+MetaverseUnknower@users.noreply.github.com>
2023-05-15 22:10:37 -0400

    Add support for an image based trigger and scaling your claim trigger

--- vlm-giveaway/trigger.ts
+++ vlm-giveaway/trigger.ts
@@ -4,7 +4,9 @@ import giveawayClaim from "./claim";
 type GiveawayOptions = {
   actionId: string;
   position: { x: number; y: number; z: number };
+  scale?: { x: number; y: number; z: number };
   glb?: string;
+  image?: string;
   clickDistance?: number;
   rotation?: number;
   hoverText?: string;
@@ -13,18 +15,31 @@ type GiveawayOptions = {
 };
 
 export function createGiveaway(options: GiveawayOptions, messageOptions: MessageOptions) {
-  const { actionId, position, glb, clickDistance, rotation, hoverText } = options;
+  const { actionId, position, scale, glb, image, clickDistance, rotation, hoverText } = options;
   const claimEntity = new Entity("Giveaway Trigger");
   engine.addEntity(claimEntity);
   if (glb) {
-    claimEntity.addComponent(new GLTFShape(glb || 'src/vlm-giveaway/VLM-Sign.glb'));
+    claimEntity.addComponent(new GLTFShape(glb || "src/vlm-giveaway/VLM-Sign.glb"));
+  } else if (image) {
+    const plane = new PlaneShape();
+    plane.uvs = [0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1];
+    claimEntity.addComponent(plane);
+    const claimImageMat = new Material();
+    const claimImageTexture = new Texture(image);
+    claimImageMat.albedoTexture = claimImageTexture;
+    claimImageMat.emissiveTexture = claimImageTexture;
+    claimImageMat.emissiveIntensity = 1;
+    claimImageMat.emissiveColor = Color3.White();
+    claimImageMat.transparencyMode = TransparencyMode.ALPHA_TEST;
+    claimEntity.addComponentOrReplace(claimImageMat);
   } else {
     claimEntity.addComponent(new BoxShape());
   }
+
   const claimEntityTransform = new Transform({
     position: new Vector3(position.x || 8, position.y || 8, position.z || 8),
     rotation: Quaternion.Euler(0, rotation || 0, 0),
-    scale: new Vector3(1, 1, 1)
+    scale: new Vector3(scale?.x || 1, scale?.y || 1, scale?.z || 1),
   });
   claimEntity.addComponentOrReplace(claimEntityTransform);
   claimEntity.addComponent(
@@ -35,7 +50,7 @@ export function createGiveaway(options: GiveawayOptions, messageOptions: Message
       {
         button: ActionButton.POINTER,
         hoverText: hoverText || "Claim Item",
-        distance: clickDistance || 5
+        distance: clickDistance || 5,
       }
     )
   );
